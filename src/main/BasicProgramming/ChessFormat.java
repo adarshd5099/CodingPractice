edd652ad7d8309651317c6ce06d934515a753df9
Adarsh D <adarsh.d@ecstvm.local>
2022-04-15 14:07:16 +0530

    Done

---
+++ src/main/BasicProgramming/ChessFormat.java
@@ -0,0 +1,40 @@
+package main.BasicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * <a href="https://www.codechef.com/problems/CHSFORMT">CHSFORMT</a>
+ * Given the time control of a chess match as a+b, determine which format of chess out of the given 4
+ * it belongs to.
+ * 1)Bullet if a+b<3
+ * 2)Blitz if 3≤a+b≤10
+ * 3)Rapid if 11≤a+b≤60
+ * 4)Classical if 60<a+b
+ */
+class ChessFormat {
+    public static void main(String[] args) throws IOException {
+        int T, a, b, result;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            String[] arr = br.readLine().split(" ");
+            a = Integer.parseInt(arr[0]);
+            b = Integer.parseInt(arr[1]);
+            if (a + b < 3)
+                result = 1;
+            else {
+                if (a + b <= 10)
+                    result = 2;
+                else {
+                    if (a + b <= 60)
+                        result = 3;
+                    else
+                        result = 4;
+                }
+            }
+            System.out.println(result);
+        }
+    }
+}
---
+++ src/main/BasicProgramming/LazyChef.java
@@ -0,0 +1,22 @@
+package main.BasicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * <a href="https://www.codechef.com/problems/LAZYCHF">LAZY CHEF</a>
+ * Chef is a very lazy person. Whatever work is supposed to be finished in x units of time, he finishes it in m∗x units of time. But there is always a limit to laziness, so he delays the work by at max d units of time. Given x,m,d, find the maximum time taken by Chef to complete the work.
+ */
+class LazyChef {
+    public static void main(String[] args) throws IOException {
+        int T, x;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            System.out.println(Math.min(arr[0] * arr[1], arr[0] + arr[2]));
+        }
+    }
+}
