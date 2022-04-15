beb6640cb5a49e49a61cb116792f82fe793daa18
Adarsh D <adarsh.d@ecstvm.local>
2022-04-15 23:37:27 +0530

    Done

---
+++ src/main/BasicProgramming/GameBetweenFriends.java
@@ -0,0 +1,40 @@
+package main.BasicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * <a href="https://www.codechef.com/problems/FRGAME">FRGAME</a>
+ * Nitin and Sobhagya were playing a game with coins. If Sobhagya has more coins then he is winning, otherwise Nitin is winning. Note that this means if both Nitin and Sobhagya have the same number of coins, then Nitin is winning.
+ * <p>
+ * Initially Nitin has A coins while Sobhagya has B coins. Then Ritik came and gave his C coins to the player who is not winning currently, after which Satyarth came and repeated the same process (gave his D
+ * <p>
+ * coins to the player who is not winning currently).
+ * <p>
+ * Find the final winner of the game.
+ */
+class GameBetweenFriends {
+    public static void main(String[] args) throws IOException {
+        int T;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            if (arr[0] >= arr[1])
+                arr[1] += arr[2];
+            else
+                arr[0] += arr[2];
+            if (arr[0] >= arr[1])
+                arr[1] += arr[3];
+            else
+                arr[0] += arr[3];
+            if (arr[0] >= arr[1])
+                System.out.println("N");
+            else
+                System.out.println("S");
+
+        }
+    }
+}
---
+++ src/main/BasicProgramming/NaiveChef.java
@@ -0,0 +1,36 @@
+package main.BasicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * <a href="https://www.codechef.com/problems/NAICHEF">NAIVE CHEF</a>
+ * Once, after a stressful day, Chef decided to relax and visit a casino near his house to gamble. He feels lucky and he's going to bet almost all of his money.
+ * <p>
+ * The game Chef is going to play in the casino consists of tossing a die with N
+ * faces twice. There is a number written on each face of the die (these numbers are not necessarily distinct). In order to win, Chef must get the number A on the first toss and the number B
+ * <p>
+ * on the second toss of the die.
+ * <p>
+ * The excited viewers want to know the probability that Chef will win the game. Can you help them find that number? Assume that Chef gets each face of the die with the same probability on each toss and that tosses are mutually independent.
+ */
+class NaiveChef {
+    public static void main(String[] args) throws IOException {
+        int T;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            int[] count = new int[100000];
+            int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int[] z = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            for (int f : z) {
+                count[f]++;
+            }
+            Double a = count[arr[1]] / Double.valueOf(arr[0]);
+            Double b = count[arr[2]] / Double.valueOf(arr[0]);
+            System.out.println(a * b);
+        }
+    }
+}
